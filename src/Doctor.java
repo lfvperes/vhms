abf3acd0d83a36814fc29b85aa7a7ce67f142599
lfvperes <luisfvperes@gmail.com>
2025-10-07 15:56:19 -0300

    feat: :sparkles: Doctor class and Species enum
    this enum contains the options of values that can be taken by the species attribute of the Patient class

---
+++ src/Doctor.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lfvperes
+ */
+class Doctor {
+    private String name;
+    private String specialization;
+    private long id;
+
+    public Doctor(String name, String specialization, long id) {
+        this.name = name;
+        this.specialization = specialization;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/Species.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author lfvperes
+ */
+public enum Species {
+    Canine,
+    Feline,
+    Avian,
+    Rodent,
+    Reptile,
+    Anfibian,
+    Fish,
+    Other
+}
