be5e32bb5ed21b274fb5ff689513c1a380449b93
lfvperes <luisfvperes@gmail.com>
2025-10-07 15:57:17 -0300

    feat: :sparkles: appt and billing classes
    classes for Appointment and Billing

---
+++ src/Appointment.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lfvperes
+ */
+class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+    private Tutor tutor;
+    private Billing billing;
+    private String report;
+
+    public Appointment(Patient patient, Doctor doctor, String date, Tutor tutor, Billing billing) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.tutor = tutor;
+        this.billing = billing;
+        this.report = ""; // Default empty report
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
+
+    public Billing getBilling() {
+        return billing;
+    }
+
+    public void setBilling(Billing billing) {
+        this.billing = billing;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+}
---
+++ src/Billing.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lfvperes
+ */
+class Billing {
+    private double amount;
+    private boolean paid;
+    private Appointment appointment;
+
+    public Billing(double amount, boolean paid, Appointment appointment) {
+        this.amount = amount;
+        this.paid = paid;
+        this.appointment = appointment;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
+}
