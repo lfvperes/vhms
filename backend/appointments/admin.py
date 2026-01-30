a7f3a037779fdbc93cdb5e98699415dfe7c3a911
lfvperes <luisfvperes@gmail.com>
2026-01-30 18:05:23 -0300

    registering appointments app to admin

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "doctor",
+        "patient",
+        "starts_at",
+        "ends_at",
+        "status",
+    )
+
+    list_filter = ("status", "doctor")
+    search_fields = ("patient__id", "doctor__id")
