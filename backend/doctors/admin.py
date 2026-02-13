94d3408d89faed301135870e3af7fc3ec96aae14
lfvperes <luisfvperes@gmail.com>
2026-02-13 14:08:25 -0300

    register doctors app to admin

--- backend/doctors/admin.py
+++ backend/doctors/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Doctor
 
-# Register your models here.
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "full_name",
+        "license_number",
+        "specialty",
+        "is_active",
+    )
+
+    list_filter = ("is_active", "specialty")
+    search_fields = ("full_name", "license_number")
