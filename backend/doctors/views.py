6e6361776703cadac78f479528fcf67812300c3e
lfvperes <luisfvperes@gmail.com>
2026-04-01 17:24:33 -0300

    feat(doctors app): :sparkles: adding search to doctors app

--- backend/doctors/views.py
+++ backend/doctors/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Doctor
 from .serializers import DoctorSerializer
 
@@ -6,6 +7,19 @@ from .serializers import DoctorSerializer
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    filter_backends = [filters.OrderingFilter]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+
+    filterset_fields = ["is_active", "specialty"]
     ordering_fields = ["full_name", "license_number", "created_at"]
     ordering = ["full_name"]
+
+    search_fields = [
+        "full_name",
+        "license_number",
+        "specialty",
+    ]
