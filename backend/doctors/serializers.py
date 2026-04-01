8844dd7455b39c6697c9f4d67072d9d7d461bf9c
lfvperes <luisfvperes@gmail.com>
2026-04-01 16:47:57 -0300

    feat(doctors app): :lock: update read-only fields and add ordering

--- backend/doctors/serializers.py
+++ backend/doctors/serializers.py
@@ -6,3 +6,4 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
--- backend/doctors/views.py
+++ backend/doctors/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Doctor
 from .serializers import DoctorSerializer
 
@@ -6,3 +6,6 @@ from .serializers import DoctorSerializer
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["full_name", "license_number", "created_at"]
+    ordering = ["full_name"]
